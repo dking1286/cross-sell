f9b7f02a858b0fe7468b0761244bb9c0e2218932
Daniel King <daniel.oliver.king@gmail.com>
2016-12-11 12:40:58 -0800

    Refactor to use switch-case statement to make logic more transparent

--- src/cross_sell.js
+++ src/cross_sell.js
@@ -1,37 +1,40 @@
 module.exports = (get) => (currentlyViewing) => {
-  const category = getCategory(currentlyViewing);
-  const generalType = getGeneralType(currentlyViewing);
+  const category = getProductCategory(currentlyViewing);
+  const compatibilityType = getCompatibilityType(currentlyViewing);
 
-  if (currentlyViewing.product_type.match(/MACHINE/)) {
-    const matchingCoffeePods = {
-      product_type: new Regexp(generalType),
-      pack_size: 12,
-    };
+  switch (category) {
+    case 'MACHINE':
+      return get('coffee_pod', {
+        product_type: new Regexp(compatibilityType),
+        pack_size: 12,
+      });
 
-    return get('coffee_pod', matchingCoffeePods);
-  } else if (currentlyViewing.product_type.match(/POD/)) {
-    const otherPackSizes = [12, 36, 60, 84].filter((size) => {
-      return size !== currentlyViewing.pack_size;
-    });
+    case 'POD':
+      const otherPackSizes = [12, 36, 60, 84].filter(size =>
+        size !== currentlyViewing.pack_size);
 
-    const matchingCoffeePods = {
-      product_type: new RegExp(generalType),
-      pack_size: { $in: otherPackSizes },
-    };
+      return get('coffee_pod', {
+        product_type: new RegExp(compatibilityType),
+        pack_size: { $in: otherPackSizes },
+      });
 
-    return get('coffee_pod', matchingCoffeePods);
+    default:
+      throw new Error(
+        `Invalid currentlyViewing data in crossSell: ${currentlyViewing}`);
   }
 };
 
-function getCategory(currentlyViewing) {
-  if (currentlyViewing.product_type.match(/COFFEE_POD/)) {
-    return 'COFFEE_POD';
-  } else if (currentlyViewing.product_type.match(/COFFEE_MACHINE/)) {
-    return 'COFFEE_MACHINE';
+function getProductCategory(currentlyViewing) {
+  if (currentlyViewing.product_type.match(/POD/)) {
+    return 'POD';
+  } else if (currentlyViewing.product_type.match(/MACHINE/)) {
+    return 'MACHINE';
   }
+
+  return null;
 }
 
-function getGeneralType(currentlyViewing) {
+function getCompatibilityType(currentlyViewing) {
   if (currentlyViewing.product_type.match(/LARGE/)) return 'LARGE';
   else if (currentlyViewing.product_type.match(/SMALL/)) return 'SMALL';
   else if (currentlyViewing.product_type.match(/ESPRESSO/)) return 'ESPRESSO';
